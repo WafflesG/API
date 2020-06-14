cd157de7a8fd5c4c06bb704ad2fba92b0932f33a
WafflesG <bennyglez@gmail.com>
2020-06-14 21:32:57 +0800

    added postgresql env variable and psycopg2

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ Created on Wed Jun 10 17:53:32 2020
 
 @author: benny
 """
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -15,7 +17,7 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ben'
 api = Api(app)
